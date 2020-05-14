e0f77a816880d346cfacef0e23452d6ad3ef7f2a
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2020-05-14 08:21:15 +0700

    .

--- app/src/main/java/com/gzeinnumer/flatmapswitchmapconcatmap/MainActivity.java
+++ app/src/main/java/com/gzeinnumer/flatmapswitchmapconcatmap/MainActivity.java
@@ -26,11 +26,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //https://www.journaldev.com/19300/rxjava-flatmap-switchmap-concatmap
         //https://medium.com/appunite-edu-collection/rxjava-flatmap-switchmap-and-concatmap-differences-examples-6d1f3ff88ee0
 
 //        flatMap();
-        switchMap();
-//        concatMap();
+//        switchMap();
+        concatMap();
 //        flatMap_x_concatMap();
     }
 
